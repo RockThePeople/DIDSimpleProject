2aba092742191801c7996c1046573c1283c415fc
rockthepeople <3qufqks@gmail.com>
2024-04-11 16:58:33 +0900

    factor : security work

--- src/App.js
+++ src/App.js
@@ -8,24 +8,12 @@ import { EthrDID } from 'ethr-did';
 import { useEffect, useState } from 'react';
 import { Resolver } from 'did-resolver'
 import { getResolver } from 'ethr-did-resolver';
+import { keypair_1, keypair_2} from './account'
 
 function App() {
   const providerConfig = { name: 'mainnet', rpcUrl: 'https://mainnet.infura.io/v3/c249e2789fff4a6db1fd3ea782ffe5a4' };
   const ethrDidResolver = getResolver(providerConfig);
   const resolver = new Resolver(ethrDidResolver);
-
-  const keypair_1 = {
-    address: '0x9021361C5226099AA99370DfeD181c9E31469d3B',
-    privateKey: '0x1de50334b47bc59027ecb6450637c333a57566941e29e51859c030de1261662b',
-    publicKey: '0x0361e0984afc0e6dbb76a2fc2f7f1e86d63613f42ce032a8e26a812c82bd9bc1e3',
-    identifier: '0x0361e0984afc0e6dbb76a2fc2f7f1e86d63613f42ce032a8e26a812c82bd9bc1e3'
-  }
-  const keypair_2 = {
-    address: '0xedf57bC0e2775433E97d85c996bB03e1dbaa57c5',
-    privateKey: '0xf0606ffc7b56978633d766bf6d4ab3ddbd441ae83d2bb85110017287960aeb2a',
-    publicKey: '0x024b6eb1a2be6962813f27864c22bc0a283ae5fffa099a7cea7eceef0b088eebf6',
-    identifier: '0x024b6eb1a2be6962813f27864c22bc0a283ae5fffa099a7cea7eceef0b088eebf6'
-  }
 
   const ethrDid_1 = new EthrDID({ ...keypair_1 })
   const ethrDid_2 = new EthrDID({ ...keypair_2 })
